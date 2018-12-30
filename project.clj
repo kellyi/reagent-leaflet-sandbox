6c73feb6c3129605b1844a955128f58710270065
Kelly Innes <kinnes@gmail.com>
2018-12-30 18:16:38 -0500

    Upgrade reitit

--- project.clj
+++ project.clj
@@ -9,9 +9,9 @@
                  [reagent "0.8.1"]
                  [alandipert/storage-atom "1.2.4"]
                  [cljs-ajax "0.8.0"]
-                 [metosin/reitit-spec "0.2.9"]
-                 [metosin/reitit-schema "0.2.9"]
-                 [metosin/reitit-frontend "0.2.9"]]
+                 [metosin/reitit-spec "0.2.10"]
+                 [metosin/reitit-schema "0.2.10"]
+                 [metosin/reitit-frontend "0.2.10"]]
 
   :plugins [[lein-cljsbuild "1.1.5"]
             [lein-figwheel "0.5.16"]
